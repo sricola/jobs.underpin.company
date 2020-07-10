9f4ba2344622a8f8d0f29f86a261cc0ec84fbc62
sricola <hi@sri.so>
2020-07-09 21:35:03 -0400

    improvements

--- js/csv_to_html_table.js
+++ js/csv_to_html_table.js
@@ -4,25 +4,23 @@ CsvToHtmlTable = {
     init: function (options) {
         options = options || {};
         var csv_path = options.csv_path || "";
-        var el = options.element || "table-container";
-        var allow_download = options.allow_download || false;
-        var csv_options = options.csv_options || {};
-        var datatables_options = options.datatables_options || {};
+        var el = "table-container";
         var custom_formatting = options.custom_formatting || [];
         var customTemplates = {};
+		
         $.each(custom_formatting, function (i, v) {
             var colIdx = v[0];
             var func = v[1];
             customTemplates[colIdx] = func;
         });
 
-        var $table = $("<table class='table table-striped table-condensed' id='" + el + "-table'></table>");
+        var $table = $("<table class='display responsive nowrap dataTable no-footer dtr-inline collapsed' width='100%' id='" + el + "-table'></table>");
         var $containerElement = $("#" + el);
         $containerElement.empty().append($table);
 
         $.when($.get(csv_path)).then(
             function (data) {
-                var csvData = $.csv.toArrays(data, csv_options);
+                var csvData = $.csv.toArrays(data);
                 var $tableHead = $("<thead></thead>");
                 var csvHeaderRow = csvData[0];
                 var $tableHeadRow = $("<tr></tr>");
@@ -50,11 +48,15 @@ CsvToHtmlTable = {
                 }
                 $table.append($tableBody);
 
-                $table.DataTable(datatables_options);
-
-                if (allow_download) {
-                    $containerElement.append("<p><a class='btn btn-info' href='" + csv_path + "'><i class='glyphicon glyphicon-download'></i> Download as CSV</a></p>");
-                }
+                $table.DataTable( {
+					paging: false, 
+    				responsive: true,
+					columnDefs: [
+						{ responsivePriority: 1, targets: 0 }
+    				]
+				});
+				
             });
+		 
     }
 };
